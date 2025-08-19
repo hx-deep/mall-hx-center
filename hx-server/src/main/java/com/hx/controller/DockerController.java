c81e5375fe2db016710798a642fd148eeb801304
gt <1803675549@qq.com>
2025-08-19 22:01:58 +0800

    接口测试

---
+++ hx-server/src/main/java/com/hx/controller/DockerController.java
@@ -0,0 +1,20 @@
+package com.hx.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RequestMapping("/hx")
+@RestController
+public class DockerController {
+
+
+    @PostMapping("getStr")
+    public String dockerMapping(@RequestBody Map<String, Object> param) {
+        Object name = param.get("name");
+        return name.toString();
+    }
+}
